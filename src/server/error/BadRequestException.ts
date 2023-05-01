1382a9c9562d491c4a99c46808186f6915fd3f4f
Mathis Burger <mburger@die-netzwerkstatt.de>
2023-05-01 19:32:20 +0200

    [TASK] Implemented accessory updates

---
+++ src/server/error/BadRequestException.ts
@@ -0,0 +1,10 @@
+class BadRequestException extends Error {
+  constructor(msg: string) {
+    super(msg);
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, BadRequestException.prototype);
+  }
+}
+
+export default BadRequestException;
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,10 @@
 import express, {Express, Request, Response} from 'express';
 import {Logger, PlatformConfig} from 'homebridge';
 import {HapClient, ServiceType} from '@oznu/hap-client';
-import {Server} from "http";
-import {Server as SocketServer} from "socket.io";
-import cors from "cors";
+import {Server} from 'http';
+import {Server as SocketServer} from 'socket.io';
+import cors from 'cors';
+import BadRequestException from './error/BadRequestException';
 import bodyParser from 'body-parser';
 
 class WebServer {
@@ -35,7 +36,7 @@ class WebServer {
       },
     });
     setInterval(async () => {
-      const data = (await this.client.getAllServices()).filter((s) => s.type !== 'ProtocolInformation');;
+      const data = (await this.client.getAllServices()).filter((s) => s.type !== 'ProtocolInformation');
       this.socket.emit('state-changed', {data});
     }, 2500);
   }
@@ -48,26 +49,73 @@ class WebServer {
 
   private static async updateService(req: Request, res: Response): Promise<void> {
     const client = res.locals.hapClient as HapClient;
-    const body = req.body;
     const all = await client.getAllServices();
-    let service: ServiceType|null = null;
-    for (let a of all) {
-      if (a.aid === body.aid && a.iid === body.iid) {
-        service = a;
-        break;
+    const service: ServiceType|undefined = all.find((s) => s.iid === req.body.iid && s.aid === req.body.aid);
+    if (!service) {
+      throw new BadRequestException('Service not found');
+    }
+    if (!service.getCharacteristic || service.serviceCharacteristics.length === 0) {
+      throw new BadRequestException('No characteristics on service');
+    }
+    const characteristic = service.getCharacteristic(service.serviceCharacteristics[0].type);
+
+    if (!characteristic || !characteristic.canWrite) {
+      const types = service.serviceCharacteristics.filter(x => x.canWrite).map(x => `'${x.type}'`).join(', ');
+      throw new BadRequestException(`Invalid characteristicType. Valid types are: ${types}.`);
+    }
+
+    let value: string|number|boolean = req.body.value;
+
+    // integers
+    if (['uint8', 'uint16', 'uint32', 'uint64'].includes(characteristic.format)) {
+      value = parseInt(value as string, 10);
+      if (characteristic.minValue !== undefined && value < characteristic.minValue) {
+        throw new BadRequestException(`Invalid value. The value must be between ${characteristic.minValue} and ${characteristic.maxValue}.`);
+      }
+      if (characteristic.maxValue !== undefined && value > characteristic.maxValue) {
+        throw new BadRequestException(`Invalid value. The value must be between ${characteristic.minValue} and ${characteristic.maxValue}.`);
       }
     }
-    // Improve update process for all types of accessories
-    if (service !== null && service !== undefined && service.getCharacteristic) {
-      const preChar = service.serviceCharacteristics.length > 0 ? service.serviceCharacteristics[0] : null;
-      if (preChar) {
-        const characteristic = await service.getCharacteristic(preChar.type);
-        if (characteristic.setValue && service.refreshCharacteristics) {
-          await characteristic.setValue(req.body.value);
-          await service.refreshCharacteristics();
+
+    // floats
+    if (characteristic.format === 'float') {
+      value = parseFloat(value as string);
+      if (characteristic.minValue !== undefined && value < characteristic.minValue) {
+        throw new BadRequestException(`Invalid value. The value must be between ${characteristic.minValue} and ${characteristic.maxValue}.`);
+      }
+      if (characteristic.maxValue !== undefined && value > characteristic.maxValue) {
+        throw new BadRequestException(`Invalid value. The value must be between ${characteristic.minValue} and ${characteristic.maxValue}.`);
+      }
+    }
+
+    // booleans
+    if (characteristic.format === 'bool') {
+      if (typeof value === 'string') {
+        if (['true', '1'].includes(value.toLowerCase())) {
+          value = true;
+        } else if (['false', '0'].includes(value.toLowerCase())) {
+          value = false;
         }
+      } else if (typeof value === 'number') {
+        value = value === 1;
+      }
+
+      if (typeof value !== 'boolean') {
+        throw new BadRequestException('Invalid value. The value must be a boolean (true or false).');
       }
     }
+
+    if (!service.refreshCharacteristics || !characteristic.setValue) {
+      throw new BadRequestException('Invalid data');
+    }
+
+    try {
+      await characteristic.setValue(value);
+      await service.refreshCharacteristics();
+    } catch (e) {
+      throw new BadRequestException('Error');
+    }
+
     const services = (await client.getAllServices()).filter((s) => s.type !== 'ProtocolInformation');
     res.send({services});
   }
